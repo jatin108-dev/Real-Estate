343ecb4117d09f6fddb478878e77f86049e133bd
Jatin Dubey <jatindubey646@gmail.com>
2026-05-04 15:26:16 +0530

    Update propertyRoutes.js

--- backend/routes/propertyRoutes.js
+++ backend/routes/propertyRoutes.js
@@ -9,6 +9,7 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   try {
     const data = await Property.find();
+    console.log("listing",data)
     res.json(data);
   } catch (err) {
     res.status(500).json({ message: "Server error" });
